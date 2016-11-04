91cdd9c5bf7c58bbeaab287e48e1500cc08c5b63
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-04 11:36:32 +0900

    Support FreeBSD (#9)
    * QA
    
    * support FreeBSD

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,6 +4,7 @@ require 'serverspec'
 
 case os[:family]
 when 'openbsd'
+
   describe file('/etc/hostname.gre0') do
     it { should be_file }
     it { should be_owned_by 'root' }
@@ -16,6 +17,23 @@ when 'openbsd'
     its(:stdout) { should match /description: GRE tunnel/ }
     its(:stderr) { should eq '' }
   end
+
+when 'freebsd'
+
+  describe file('/etc/rc.conf') do
+    it { should be_file }
+    its(:content) { should match(/#{ Regexp.escape('cloned_interfaces="${cloned_interfaces} gre0"') }/) }
+    its(:content) { should match(/#{ Regexp.escape('ifconfig_gre0="inet 10.0.2.15 10.0.2.100 tunnel 192.168.1.100 192.168.2.100 grekey MY_GRE_KEY"') }/) }
+  end
+
+  describe command("ifconfig gre0") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/^gre0: flags=\d+<UP,POINTOPOINT,RUNNING,MULTICAST> metric 0 mtu \d+$/) }
+    its(:stdout) { should match(/^\s+tunnel inet #{ Regexp.escape('192.168.1.100') } --> #{ Regexp.escape('192.168.2.100') }$/) }
+    its(:stdout) { should match(/^\s+inet #{ Regexp.escape('10.0.2.15') } --> #{ Regexp.escape('10.0.2.100') } netmask 0x0/) }
+    its(:stderr) { should match(/^$/) }
+  end
+
 else
   raise "unsupported OS #{ os[:family] }"
 end
